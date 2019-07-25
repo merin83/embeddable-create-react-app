bf1750498592427163ac64fd4de0183f5ceb9190
merin83 <omi.mychoice@yahoo.com>
2019-07-25 16:42:24 +0600

    copy file issue fixed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const webpackConfig = `
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob');
+
+module.exports = {
+  entry: {
+    'bundle.js': glob
+      .sync('build/static/?(js|css|media)/*.?(js|css|png|woff|woff2|eot|ttf|svg)')
+      .map(f => path.resolve(__dirname, f)),
+  },
+  output: {
+    filename: 'build/static/js/bundle.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: ['url-loader?limit=100000'] }
+    ],
+  },
+  plugins: [new UglifyJsPlugin()],
+};
+`;
+
 (async () => {
   const { option } = await inquirer.prompt([
     {
@@ -13,19 +40,8 @@ const fs = require('fs');
   ]);
   if (option) {
     const cwd = process.cwd();
-    fs.copyFile(
-      'src/config/webpack.config.js',
-      `${cwd}/webpack.config.js`,
-      err => {
-        if (err) {
-          console.log(
-            'Make sure you have Node version 8.5 or higher installed on your system'
-          );
-          throw err;
-        }
-        console.log('webpack.config.js successfully created');
-      }
-    );
+    fs.writeFileSync(`${cwd}/webpack.config.js`, webpackConfig);
+    console.log('webpack.config.js successfully created');
   } else {
     console.log(`webpack.config.js creation failed choose 'y' to create it`);
   }
